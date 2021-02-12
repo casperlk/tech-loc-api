5c26dd20e6df1d1006ada2efc494e1bfcb383c8f
Casper L'Esperance-Kerckhoff <casperlesperancekerckhoff@gmail.com>
2021-02-12 14:15:07 -0600

    Commented, removed old code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import json, time
-from flask import Flask, jsonify, Response, request
+import json
+from flask import Flask, jsonify
 import haversine as hs
 from flask_cors import CORS
 
@@ -7,11 +7,11 @@ app = Flask(__name__)
 CORS(app)
 cors = CORS(app, resources={
     r"/*": {
-        "origins": "*"
+        "origins": "*" # set to * for testing, in deployment set to front end server address so that bad actors can't access loc data
     }
 })
 
-maxTimeDiff = 0     # maximum permissible time difference for two location reports for techs to be considered colocated (in seconds, within a given payload)
+maxTimeDiff = 0     # maximum permissible time difference of two location reports for techs to be considered colocated (in seconds, within a given payload)
 reportIndex = 0     # Index of current technician location geojson
 apiVersionNumber = '1'
 farmID = '000001'   # unique identifier for the solar farm
@@ -35,10 +35,9 @@ def query_tech_loc():
     reportIndex = reportIndex + 1 if reportIndex < (numberOfReports - 1) else 0
     return jsonify(payload)
 
-
+# returns an array containing pairs of technicians whose distance is less than 1000 feet
 def findColocatedTechs(techReport):
     techPairs = []      # techPairs contains pairs of indices of colocated technicians
-    
     global maxTimeDiff
 
     numberOfTechs = len(techReport['features'])
@@ -50,8 +49,9 @@ def findColocatedTechs(techReport):
             if diffTime <= maxTimeDiff:
                 loc1 = techReport['features'][i]['geometry']['coordinates']
                 loc2 = techReport['features'][j]['geometry']['coordinates']
+                # haversine computes distance in km accounting for curve of earth
                 if hs.haversine(loc1,loc2) < 0.3048:
-                    techPairs.append([techReport['features'][i]['properties']['name'],techReport['features'][j]['properties']['name']])
+                    techPairs.append([techReport['features'][i]['properties']['name'], techReport['features'][j]['properties']['name']])
     return techPairs
 
 if __name__ == "__main__":
